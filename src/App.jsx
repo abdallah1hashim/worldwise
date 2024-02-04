4e3a75b156f14b54af97cfcbbc6b5add8ef4a7b6
Inulla <b21ahahim@gmail.com>
2024-02-04 23:10:54 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import "./index.css";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://mocki.io/v1/a4cdc8dc-8c1f-48fd-b2dd-850971ab4ad2`
+        );
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was an error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
